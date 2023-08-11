8eb2b6702ce76fca6f230a363ac58bb656b1e00d
sandeshkanere <sandeshkanere@gmail.com>
2023-08-11 12:45:39 +0530

    small change

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     if(req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}:${req.url} New request received!\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New request received!\n`;
+  const myUrl = url.parse(req.url,true);
+  console.log(myUrl);
   fs.appendFile("./log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("home page");
         break;
+      case "/signup":
+        if(req.method === "GET") res.end("This is sign up form");
+        else if (req.method === "POST") {
+          //DB Query
+          res.end("Success");
+        }
       case "/about":
-        res.end("about page");
+        const username = myUrl.query.myname;
+        res.end(`Hi,${username}`);
         break;
       case "/contact":
         res.end("contact page");

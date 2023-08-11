ff76cafa069201c7a6483c6f61eacb0a07d992fc
sandeshkanere <sandeshkanere@gmail.com>
2023-08-11 14:48:08 +0530

    Added dependencies

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.method} ${req.url} New request received!\n`;
-  const myUrl = url.parse(req.url,true);
-  console.log(myUrl);
-  fs.appendFile("./log.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        res.end("home page");
-        break;
-      case "/signup":
-        if(req.method === "GET") res.end("This is sign up form");
-        else if (req.method === "POST") {
-          //DB Query
-          res.end("Success");
-        }
-      case "/about":
-        const username = myUrl.query.myname;
-        res.end(`Hi,${username}`);
-        break;
-      case "/contact":
-        res.end("contact page");
-        break;
-
-      default:
-        res.end("404 not found");
-    }
-  });
+app.get("/",(req,res)=>{
+  return res.send("Hello from home page" + " hey " + req.query.name + " And you are " + req.query.age);
 });
 
+app.get("/about",(req,res)=>{
+  return res.send("Hello from about page");
+});
+
+const myServer = http.createServer(app);
 myServer.listen(8000, () => console.log("Server started!"));
